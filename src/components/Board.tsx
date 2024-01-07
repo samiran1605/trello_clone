675eb1819f0cd6cbbb04d402047a00726a11f600
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2024-01-07 23:09:03 +0530

    appwrite config_1

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
-  return <div>Board</div>;
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div className="">{/* rendering all the columns */}</div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default Board;
