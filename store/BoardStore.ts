18cf1abc1422b2f724dacb2fa68d2098c6c42951
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2024-01-13 16:50:40 +0530

    columns are made and displayed

---
+++ store/BoardStore.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import { getTodosGroupedByColumn } from "../lib/getTodosGroupedByColumn";
+
+interface BoardState {
+  board: Board;
+  getBoard: () => void;
+}
+
+export const useBoardStore = create((set) => ({
+  board: {
+    columns: new Map<TypedColumn, Column>(),
+  },
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
+}));
