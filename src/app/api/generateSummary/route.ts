1507aa95ec182244163abdf94a4d76a802247e9f
SAMIRAN <119873097+samiranqwerty@users.noreply.github.com>
2024-01-31 21:28:08 +0530

    chat GPT added

---
+++ src/app/api/generateSummary/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import openai from "../../../../openai";
+
+export async function POST(request: Request) {
+  const { todos } = await request.json();
+  console.log(todos);
+
+  // communicate with openAI GPT-4
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: "system",
+        content: `When responding, welcome the user always as Mr.Samiran and say welcome to the Loyal Honey App! Limit the response to 200 characters`,
+      },
+      { role: "user", content: "Who won the world series in 2020?" },
+      {
+        role: "user",
+        content: `Hi there, provided a summary of the following todos. Count how many todos are in each category such as To do, in progress and done, then tell the user to have a productive day! Here's the data: ${JSON.stringify(
+          todos
+        )}`,
+      },
+    ],
+  });
+
+  console.log("DATA IS", response);
+  console.log(response.choices[0].message);
+  return NextResponse.json(response.choices[0].message);
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "../../store/BoardStore";
+import fetchSuggestion from "../../lib/fetchSuggestion";
 
 function Header() {
-  const [searchString, setSearchString] = useBoardStore((state) => [
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
     state.searchString,
     state.setSearchString,
   ]);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("");
+
+  useEffect(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+    fetchSuggestionFunc();
+  }, [board]);
 
   return (
     <header className="">
@@ -45,8 +62,14 @@ function Header() {
       </div>
       <div className="flex items-center justify-center px-5 md:py-5">
         <p className="flex items-center text-sm p-2 font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-          GPT summary for the day...
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
+              loading && "animate-spin"
+            }`}
+          />
+          {suggestion && !loading
+            ? suggestion
+            : "GPT is summarizing your task for the day..."}
         </p>
       </div>
     </header>
